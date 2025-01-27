98426644f03cbec0f763ddcd335162c130f6f5d5
SamSagace <74056270+SamTheSagace@users.noreply.github.com>
2025-01-27 18:14:09 +0100

    carrousel number function and css

--- src/components/Carroussel/Carroussel.jsx
+++ src/components/Carroussel/Carroussel.jsx
@@ -6,12 +6,14 @@ function Carroussel({imgURL}){
     const max = imgURL.length-1
     return(
     <>
+    
     <div className="carroussel">
-        <div className='carrousselControl'>
+        {imgURL.length > 1 ? <div className='carrousselControl'>
             <button onClick={() => setNumber(number > 0 ? number -1 : max)}><i className="fa-solid fa-angle-left" ></i></button>
+            <div className='numbers'><p>{number+1}/{max+1}</p></div>
             <button onClick={() => setNumber(number < max  ? number +1 : 0)}><i className="fa-solid fa-angle-right"></i></button>
-            <p>{number+1}/{max+1}</p>
-        </div>        
+            
+        </div>  : ''}              
         <img src={imgURL[number]} alt="" />
     </div>
     </>
